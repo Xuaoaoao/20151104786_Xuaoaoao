17374c69275c55eb223028305b0a8f5e40fb94ef
Xuaoaoao <610166025@qq.com>
2018-12-23 19:08:13 +0800

    实体类

---
+++ 20151104786_Xuao_Order/src/main/java/com/zs/LoginDemo/model/UserInfo.java
@@ -0,0 +1,47 @@
+package com.zs.LoginDemo.model;
+
+public class UserInfo {
+	private Integer userid;
+	private String username;
+	private String password;
+	private Integer usermoney;
+	private String useradss;
+	private String userphone;
+	public Integer getUserid() {
+		return userid;
+	}
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getUsermoney() {
+		return usermoney;
+	}
+	public void setUsermoney(Integer usermoney) {
+		this.usermoney = usermoney;
+	}
+	public String getUseradss() {
+		return useradss;
+	}
+	public void setUseradss(String useradss) {
+		this.useradss = useradss;
+	}
+	public String getUserphone() {
+		return userphone;
+	}
+	public void setUserphone(String userphone) {
+		this.userphone = userphone;
+	}
+	
+}
